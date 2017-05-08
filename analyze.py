2b8f74fdd5a514f5db0f56d73a4fc1397c7997b1
Antonino Ingargiola <tritemio@gmail.com>
2017-05-08 10:57:43 -0700

    Print which data file is being analyzed

--- analyze.py
+++ analyze.py
@@ -7,6 +7,8 @@ default_notebook_name = 'smFRET-Quick-Test-Server.ipynb'
 def run_notebook(data_filename, input_notebook=None):
     if input_notebook is None:
         input_notebook = default_notebook_name
-    nbrun.run_notebook(input_notebook,
+    print(' * Running analysis for %s' % (data_filename.stem), flush=True)
+    nbrun.run_notebook(input_notebook, display_links=False,
                        out_notebook_path=data_filename.with_suffix('.ipynb'),
                        nb_kwargs={'fname': str(data_filename)})
+    print('   [COMPLETED ANALYSIS] %s' % (data_filename.stem), flush=True)
