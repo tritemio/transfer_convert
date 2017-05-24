25d213e6893ee7292408fe283dbd9786caf7df48
Antonino Ingargiola <tritemio@gmail.com>
2017-05-24 14:24:52 -0700

    Add commad-line interface to analyze.py

--- analyze.py
+++ analyze.py
@@ -24,3 +24,31 @@ def run_analysis(data_filename, input_notebook=None, save_html=False,
                            nb_kwargs={'fname': str(data_filename)},
                            save_html=save_html, working_dir=working_dir)
     print('   [COMPLETED ANALYSIS] %s' % (data_filename.stem), flush=True)
+
+
+if __name__ == '__main__':
+    import argparse
+    from pathlib import Path
+
+    descr = """\
+        This script executes an analysis notebook on the specified HDF5 file.
+        """
+    parser = argparse.ArgumentParser(description=descr, epilog='\n')
+    parser.add_argument('datafile',
+                        help='Source folder with files to be processed.')
+    msg = ("Filename of the analysis notebook. If not specified, the default "
+           "notebook is '%s'." % default_notebook_name)
+    parser.add_argument('--notebook', metavar='NB_NAME',
+                        default=default_notebook_name, help=msg)
+    parser.add_argument('--save-html', action='store_true',
+                        help='Save a copy of the output notebooks in HTML.')
+    parser.add_argument('--working-dir', metavar='PATH', default='./',
+                        help='Working dir for the kernel executing the notebook.')
+    args = parser.parse_args()
+
+    datafile = Path(args.datafile)
+    assert datafile.is_file(), 'Data file not found: %s' % datafile
+    notebook = Path(args.notebook)
+    assert notebook.is_file(), 'Notebook not found: %s' % notebook
+    run_analysis(datafile, input_notebook=notebook,
+                 save_html=args.save_html, working_dir=args.working_dir)
